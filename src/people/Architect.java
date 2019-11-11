6f7c2f60d0b2df0a41850c7308cf8331aa455c06
oquzhax <oguzhanozer90@gmail.com>
2019-11-12 00:08:12 +0300

    architech and person constructor

---
+++ src/people/Architect.java
@@ -0,0 +1,11 @@
+package people;
+
+import utility.Date;
+
+public class Architect extends Person {
+
+	public Architect(String name, Date birthDate, Date deathDate, String nationality) {
+		super(name, birthDate, deathDate, nationality);
+	}
+	
+}
--- src/people/Person.java
+++ src/people/Person.java
@@ -7,7 +7,14 @@ public abstract class Person {
 	private Date birthDate;
 	private Date deathDate;
 	private String nationality;
-
+	
+	public Person(String name, Date birthDate, Date deathDate, String nationality) {
+		this.name = name;
+		this.birthDate = birthDate;
+		this.deathDate = deathDate;
+		this.nationality = nationality;
+	}
+	
 	public String getName() {
 		return name;
 	}
