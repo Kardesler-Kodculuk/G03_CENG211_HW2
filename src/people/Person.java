57cc9dbe2b9c915bd08507ca0b268f7c1a36f301
oquzhax <oguzhanozer90@gmail.com>
2019-11-13 01:00:48 +0300

    Update Person.java

--- src/people/Person.java
+++ src/people/Person.java
@@ -1,6 +1,10 @@
 package people;
 
-public abstract class Person {
+import java.security.InvalidParameterException;
+
+import utility.IComparable;
+
+public abstract class Person implements IComparable<Person>{
 	private String name;
 	private int birthDate;
 	private int deathDate;
@@ -24,5 +28,32 @@ public abstract class Person {
 	}
 	public String getNationality() {
 		return nationality;
+	}
+	
+	@Override
+	public int compareTo(Person other, String key) {
+		try {
+			if(key == "name") {
+				return this.name.compareTo(other.getName());
+			}
+			else if(key == "birthDate") {
+				if(this.birthDate < other.getBirthDate()) {return -1;}
+				else if(this.birthDate > other.getBirthDate()) {return 1;}
+				else {return 0;}
+			}
+			else if(key == "nationality") {
+				return this.nationality.compareTo(other.getNationality());
+			}
+			else {
+				throw new InvalidParameterException();
+			}
+
+		}
+		catch(NullPointerException e) {
+			throw e;
+		}
+		catch(ClassCastException e) {
+			throw e;
+		}
 	}
 }
