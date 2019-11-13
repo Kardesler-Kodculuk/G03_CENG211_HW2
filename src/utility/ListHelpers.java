1d95630e1217e5cbb7cdf93ce345069f7d8f0cfa
oquzhax <oguzhanozer90@gmail.com>
2019-11-13 19:15:29 +0300

    Update ListHelpers.java

--- src/utility/ListHelpers.java
+++ src/utility/ListHelpers.java
@@ -3,6 +3,25 @@ package utility;
 import java.util.List;
 
 public class ListHelpers {
-	public static <T> void sort(List<T> list, String key){
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void sort(List<IComparable> list, String key){
+		int len = list.size();
+		for(int i = 0; i < len; i++) { // i - index for main loop
+			for(int j = 0; j < len - i - 1; j++) { // j - index for comparisons
+				IComparable first = list.get(j);
+				IComparable second = list.get(j + 1);
+				if(first.compareTo(second, key) > 0) {
+					swap(list, j, j+1);
+				}
+			}
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	private static void swap(List<IComparable> list, int i1, int i2) {
+		IComparable element1 = list.remove(i1);
+		IComparable element2 = list.remove(i2);
+		list.add(i1, element2);
+		list.add(i2, element1);
 	}
 }
