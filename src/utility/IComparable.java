c3e5c46cbd82f33dfdbccf386be668175e968249
oquzhax <oguzhanozer90@gmail.com>
2019-11-14 23:23:04 +0300

    update comments in comparable

--- src/utility/IComparable.java
+++ src/utility/IComparable.java
@@ -5,7 +5,7 @@ public interface IComparable<T extends IComparable<? extends T>> {
 	 * Compares the object with another with respect to a key
 	 * @param other - other object
 	 * @param key - compare according to this
-	 * @return
+	 * @return - 1 if this object bigger, 0 if they equal, -1 if other object bigger
 	 * @throws IllegalArgumentException - when it takes invalid argument
 	 */
 	public int compareTo(T other, String key) throws IllegalArgumentException;
