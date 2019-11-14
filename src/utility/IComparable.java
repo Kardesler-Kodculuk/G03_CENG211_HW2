365a8b38cf3db1d93cf8653bee3e12e232c9a6eb
oquzhax <oguzhanozer90@gmail.com>
2019-11-14 23:15:52 +0300

    commented IComparable

--- src/utility/IComparable.java
+++ src/utility/IComparable.java
@@ -2,11 +2,11 @@ package utility;
 
 public interface IComparable<T extends IComparable<? extends T>> {
 	/**
-	 * Compares the object with another with respect to a key which is
-	 * @param other
-	 * @param key
+	 * Compares the object with another with respect to a key
+	 * @param other - other object
+	 * @param key - compare according to this
 	 * @return
-	 * @throws IllegalArgumentException
+	 * @throws IllegalArgumentException - when it takes invalid argument
 	 */
 	public int compareTo(T other, String key) throws IllegalArgumentException;
 }
