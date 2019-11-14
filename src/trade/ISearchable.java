4e7f257878ea8c02cc7488ac348b6833f816bfb5
oquzhax <oguzhanozer90@gmail.com>
2019-11-14 23:20:09 +0300

    commented ISearchable

--- src/trade/ISearchable.java
+++ src/trade/ISearchable.java
@@ -1,5 +1,10 @@
 package trade;
 
 public interface ISearchable {
+	/**
+	 * Searchs for the querry
+	 * @param querry - searches according to querry
+	 * @return - true if found, false if not
+	 */
 	public boolean search(String querry);
 }
